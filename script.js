1cc618e13446fbfc3deee0c18193e3930fb11aac
abby <abby@abbymclean.com>
2023-09-12 22:54:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('emailForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log(JSON.stringify({ email: document.getElementById('email').value }))
+  fetch('https://script.google.com/macros/s/AKfycbw2ohfU9R65oUAii7e-J7YU6oopL6_heHNn2JoFRxtf1QAaeanCd0fk0HycrGm9oC2I/exec', {
+    method: 'POST',
+    mode: 'no-cors',
+    redirect: 'follow',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({ email: document.getElementById('email').value })
+  }).then(function(response) {
+    console.log(response)
+    if (response.status == 0) {
+      document.getElementById('emailForm').innerHTML = '<p>Thanks for subscribing!</p>';
+      setTimeout(function() {
+        var element = document.getElementById('emailForm');
+        element.parentNode.removeChild(element);
+      }, 3000);
+    }
+  })
+    .catch(function(error) {
+      console.log(error)
+      alert('Failed to subscribe');
+    });
+});
